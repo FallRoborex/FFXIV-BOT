033841a6922750c44fe0599df3b068f69283c39c
FallRoborex <robertoramirezgalan04@gmail.com>
2024-05-22 11:51:31 -0400

    Fixed my format

--- src/Discord bot/response.py
+++ src/Discord bot/response.py
@@ -1,8 +1,9 @@
 from random import choice, randint
 
+
 def get_response(user_input: str) -> str:
     lowered = str(user_input.lower())
-    
+
     if lowered == "":
         return "Diogo is old!!!!"
     elif "hello" in lowered:
