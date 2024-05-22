56f2979e588efcda5b622ccff9388df13963f044
FallRoborex <robertoramirezgalan04@gmail.com>
2024-05-22 02:29:58 -0400

    Created the bot needs to be able to check for messages now

--- src/Discord bot/discord_bot.py
+++ src/Discord bot/discord_bot.py
@@ -1,6 +1,56 @@
 import os
 from discord import Intents, Client, Message
 from dotenv import load_dotenv
-from responce import get_response
+from responce import get_responce
+
+# Load the Token 
+load_dotenv()
+TOKEN = str(os.getenv("DISCORD_TOKEN"))
 
 
+# Bot Setup
+intents = Intents.default()
+Intents.message_content = True
+client = Client(intents=intents)
+
+# Message Functionality
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print("(Message was empty)")
+        return
+    
+    is_private = user_message[0] == "?"
+    if is_private:
+        user_message = user_message[1:]
+    
+    try:
+        response = str(get_responce(user_message))
+        await message.author.send(response) if is_private else message.channel.send(response)
+    except Exception as e:
+        print(e)
+        
+
+@client.event
+async def on_ready() -> None:
+    print(f"{client.user} is now running!")
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+    
+    print(f"[{channel}] {username}: \"{user_message}\"")
+    
+    
+
+def main() -> None:
+    client.run(token=TOKEN)
+    
+if __name__ == "__main__":
+    main()
+        
+        
--- src/Discord bot/responce.py
+++ src/Discord bot/responce.py
@@ -1,2 +1,13 @@
+from random import choice, randint
+
 def get_responce(user_input: str) -> str:
-    raise NotImplementedError("Code is missing... ")
+    lowered = str(user_input.lower())
+    
+    if lowered == "":
+        return "Diogo is old!!!!"
+    elif "hello" in lowered:
+        return "Hello world, bitch!"
+    elif "roll dice" in lowered:
+        return f"You rolled: {randint(1, 20)}"
+    else:
+        return choice(["I don't understand what the hell you are talking about."])
