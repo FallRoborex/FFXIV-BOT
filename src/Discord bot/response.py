9a623ff4b33c89a9253f1d7ab4626ef96c6665f8
FallRoborex <robertoramirezgalan04@gmail.com>
2024-05-23 09:37:08 -0400

    Added the ability to search for a mount

--- src/Discord bot/response.py
+++ src/Discord bot/response.py
@@ -1,17 +1,36 @@
 from random import choice, randint
+import requests
 
+
+def fetch_mount():
+    url = "https://ffxivcollect.com/api/mounts"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return "Error fetching mounts data"
+
+
+def search_for_mount(name):
+    mounts = fetch_mount()
+
+    if mounts and isinstance(mounts, dict):
+
+        mounts_list = mounts.get('results', [])
+        for mount in mounts_list:
+            if name.lower() in mount['name'].lower():
+                return f"Name: {mount['name']}\nID: {mount['id']}\nSource: {mount['sources']}"
+    return "Mount not found"
 
 
 def get_response(user_input: str) -> str:
-    lowered = str(user_input.lower())
+    lowered = str(user_input.lower()).split()
+    command = lowered.pop(0)
+    item = " ".join(lowered)
 
-    if lowered == "":
-        return "Diogo is old!!!!"
-    elif "hello" in lowered:
-        return "Hello world, bitch!"
-    elif "roll dice" in lowered:
-        return f"You rolled: {randint(1, 20)}"
-    elif "how old is Diogo":
-        return "Older then the milky way"
+    if "mount" in command:
+        return search_for_mount(item)
+
     else:
-        return choice(["I don't understand what the hell you are talking about."
+        return choice(["I don't understand what the hell you are talking about."])
